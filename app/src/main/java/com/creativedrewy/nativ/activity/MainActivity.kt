c0cf7412598fe08475f4fb99c922308359e89235
Andrew Watson <creativedrewy@gmail.com>
2021-07-06 19:50:57 -0700

    Finish adding hilt infra

--- app/src/main/java/com/creativedrewy/nativ/activity/MainActivity.kt
+++ app/src/main/java/com/creativedrewy/nativ/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.creativedrewy.nativ
+package com.creativedrewy.nativ.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,15 +9,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.creativedrewy.nativ.ui.theme.NATIVTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             NATIVTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
                     Greeting("Android")
                 }
             }
