fd961142a9c508e67f8be3f7af3bf0a67d44ba13
Andrew Watson <creativedrewy@gmail.com>
2021-07-06 20:08:57 -0700

    Introduce ViewModel

--- app/src/main/java/com/creativedrewy/nativ/activity/MainActivity.kt
+++ app/src/main/java/com/creativedrewy/nativ/activity/MainActivity.kt
@@ -3,16 +3,21 @@ package com.creativedrewy.nativ.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.creativedrewy.nativ.ui.theme.NATIVTheme
+import com.creativedrewy.nativ.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
