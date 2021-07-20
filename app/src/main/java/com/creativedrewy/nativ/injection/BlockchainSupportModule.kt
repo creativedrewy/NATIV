6be87bbe7c94ec177d4c10a73eee3d30bda7f8bc
Andrew Watson <creativedrewy@gmail.com>
2021-07-19 23:30:04 -0700

    Actually add supported chains and their respective loading classes to the module.

--- app/src/main/java/com/creativedrewy/nativ/injection/BlockchainSupportModule.kt
+++ app/src/main/java/com/creativedrewy/nativ/injection/BlockchainSupportModule.kt
@@ -1,9 +1,11 @@
 package com.creativedrewy.nativ.injection
 
+import com.creativedrewy.nativ.R
 import com.creativedrewy.nativ.chainsupport.IBlockchainNftLoader
 import com.creativedrewy.nativ.chainsupport.ISupportedChains
 import com.creativedrewy.nativ.chainsupport.SupportedChain
 import com.creativedrewy.nativ.metaplex.MetaplexNftUseCase
+import com.creativedrewy.nativ.opensea.OpenSeaQueryUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,10 +19,22 @@ class BlockchainSupportModule {
 
     @Provides
     fun providesISupportedChains(
-        metaplex: MetaplexNftUseCase
+        metaplexLoaderUseCase: MetaplexNftUseCase,
+        openSeaLoaderUseCase: OpenSeaQueryUseCase
     ): ISupportedChains {
         return object : ISupportedChains {
-            override val chainsToNftLoadersMap: Map<SupportedChain, IBlockchainNftLoader> = mapOf()
+            override val chainsToNftLoadersMap: Map<SupportedChain, IBlockchainNftLoader> = mapOf(
+                SupportedChain(
+                    name = "Solana",
+                    ticker = "SOL",
+                    iconRes = R.drawable.solana_logo
+                ) to metaplexLoaderUseCase,
+                SupportedChain(
+                    name = "Ethereum",
+                    ticker = "ETH",
+                    iconRes = R.drawable.eth_diamond_black
+                ) to openSeaLoaderUseCase
+            )
         }
     }
 
