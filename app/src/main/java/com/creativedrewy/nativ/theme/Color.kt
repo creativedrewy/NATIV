98945b1ae72b9e56f3f3206d468bcee2a98755c6
Andrew Watson <creativedrewy@gmail.com>
2021-07-23 16:45:30 -0700

    Bring in official colors and wire up to dark theme.

--- app/src/main/java/com/creativedrewy/nativ/theme/Color.kt
+++ app/src/main/java/com/creativedrewy/nativ/theme/Color.kt
@@ -2,10 +2,12 @@ package com.creativedrewy.nativ.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkestBlue = Color(0xFF0c1e3e)
+val DarkBlue = Color(0xFF122c5a)
+val Turquoise = Color(0xFF38fbdb)
+val HotPink = Color(0xFFfc0ff5)
+val LightPurple = Color(0xFF8e52f5)
+
 
 val ShimmerColor = Color(0.92f, 0.92f, 0.92f)
 val White = Color(1f, 1f, 1f)
--- app/src/main/java/com/creativedrewy/nativ/theme/Theme.kt
+++ app/src/main/java/com/creativedrewy/nativ/theme/Theme.kt
@@ -3,28 +3,16 @@ package com.creativedrewy.nativ.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    primary = DarkestBlue,
+    primaryVariant = DarkBlue,
+    secondary = HotPink,
+    surface = DarkBlue,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onSurface = Color.White
 )
 
 @Composable
@@ -32,11 +20,8 @@ fun NATIVTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    //For now NATIV is only dark theme
+    val colors = DarkColorPalette
 
     MaterialTheme(
         colors = colors,
