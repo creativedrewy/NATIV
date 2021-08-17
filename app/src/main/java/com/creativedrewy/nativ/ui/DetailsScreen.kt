423328c988e0068c5b67ade0c94b1e26fc0ea353
Andrew Watson <creativedrewy@gmail.com>
2021-08-16 21:50:19 -0700

    Bring in the new bg assets and put perspective grid in

--- app/src/main/java/com/creativedrewy/nativ/ui/DetailsScreen.kt
+++ app/src/main/java/com/creativedrewy/nativ/ui/DetailsScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -53,13 +52,12 @@ fun DetailsScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(320.dp)
-                .background(Color.Gray)
+                .background(MaterialTheme.colors.primaryVariant)
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(220.dp)
-                    .background(MaterialTheme.colors.primary)
                     .align(Alignment.BottomCenter)
             ) {
                 Image(
@@ -67,7 +65,7 @@ fun DetailsScreen(
                         .fillMaxWidth()
                         .height(220.dp),
                     painter = painterResource(
-                        id = R.drawable.perspective_grid
+                        id = R.drawable.perspective_grid_variant
                     ),
                     contentScale = ContentScale.FillHeight,
                     contentDescription = ""
