ff93df77f807c718b198f9a6bafd415fed90bbdf
Andrew <creativedrewy@gmail.com>
2023-02-15 12:55:39 -0800

    Remove support for eth & harmony modules at the root app level. No longer options in the app.

--- app/src/main/java/com/creativedrewy/nativ/injection/BlockchainSupportModule.kt
+++ app/src/main/java/com/creativedrewy/nativ/injection/BlockchainSupportModule.kt
@@ -4,8 +4,6 @@ import com.creativedrewy.nativ.R
 import com.creativedrewy.nativ.chainsupport.IBlockchainNftLoader
 import com.creativedrewy.nativ.chainsupport.ISupportedChains
 import com.creativedrewy.nativ.chainsupport.SupportedChain
-import com.creativedrewy.nativ.ethereumnft.OpenSeaQueryUseCase
-import com.creativedrewy.nativ.harmonyonenft.HarmonyNftUseCase
 import com.creativedrewy.solananft.metaplex.MetaplexNftUseCase
 import dagger.Module
 import dagger.Provides
@@ -20,9 +18,7 @@ class BlockchainSupportModule {
 
     @Provides
     fun providesISupportedChains(
-        metaplexLoaderUseCase: MetaplexNftUseCase,
-        openSeaLoaderUseCase: OpenSeaQueryUseCase,
-        harmonyLoaderUseCase: HarmonyNftUseCase
+        metaplexLoaderUseCase: MetaplexNftUseCase
     ): ISupportedChains {
         return object : ISupportedChains {
             override val chainsToNftLoadersMap: Map<SupportedChain, IBlockchainNftLoader> = mapOf(
@@ -30,17 +26,7 @@ class BlockchainSupportModule {
                     name = "Solana",
                     ticker = "SOL",
                     iconRes = R.drawable.solana_logo
-                ) to metaplexLoaderUseCase,
-                SupportedChain(
-                    name = "Ethereum",
-                    ticker = "ETH",
-                    iconRes = R.drawable.eth_diamond_black
-                ) to openSeaLoaderUseCase,
-                SupportedChain(
-                    name = "Harmony One",
-                    ticker = "ONE",
-                    iconRes = R.drawable.harmony_logo
-                ) to harmonyLoaderUseCase
+                ) to metaplexLoaderUseCase
             )
         }
     }
