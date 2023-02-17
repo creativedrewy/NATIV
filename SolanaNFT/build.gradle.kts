7e9243bf8a1b2f87c76f8bd7e942626a85b8984d
Andrew <creativedrewy@gmail.com>
2023-02-17 11:40:13 -0800

    Refactor one of the gradle files to kts

--- SolanaNFT/build.gradle.kts
+++ SolanaNFT/build.gradle.kts
@@ -1,48 +1,47 @@
 plugins {
-    id 'com.android.library'
-    id 'org.jetbrains.kotlin.android'
-    id 'kotlin-kapt'
-    id 'com.google.dagger.hilt.android'
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
-    compileSdk 33
-    buildToolsVersion "31.0.0"
+    compileSdk = 33
+    buildToolsVersion = "31.0.0"
 
     defaultConfig {
-        minSdk 21
-        targetSdk 33
+        minSdk = 21
+        targetSdk = 33
 
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
-
-        consumerProguardFiles "consumer-rules.pro"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation project(path: ':ChainSupport')
+    implementation(project(":ChainSupport"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 
     implementation("com.github.metaplex-foundation:metaplex-android:1.4.1") {
-        exclude group: 'com.github.metaplex-foundation.kborsh', module: 'kborsh-jvm'
+        exclude(group = "com.github.metaplex-foundation.kborsh", module = "kborsh-jvm")
     }
 
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
