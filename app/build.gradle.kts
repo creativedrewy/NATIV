321ecec456af651ae49b1032b13eeb6f962340ed
Andrew <creativedrewy@gmail.com>
2026-03-07 21:29:45 -0800

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "0.8.0"
+        versionName = "0.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
